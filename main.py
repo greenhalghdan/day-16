00fc2cd61fed5aff39b66c74dcdf90fc811e4521
daniel <greenhalghdan@gmail.com>
2023-05-24 22:11:54 +0100

    day16

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import another_module
 
 from turtle import Turtle, Screen
+#
+# print(another_module.another_variable)
+#
+# #creating a new object
+# timmy = Turtle()
+# print(timmy)
+#
+# my_screen = Screen()
+# timmy.shape("turtle")
+# timmy.color("blue2")
+# timmy.forward(100)
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+from prettytable import PrettyTable
 
-print(another_module.another_variable)
+table = PrettyTable()
+table.field_names = ["Ipswich Player names", "Type"]
+table.add_rows(
+    [
+        ["Conor Chaplin", "Forward"],
+        ["Freddie Ladapo", "Forward"],
+        ["Leif Davis", "Left Back"]
+    ]
+)
+table.align ="r"
+print(table)
 
-#creating a new object
-timmy = Turtle()
-print(timmy)
-
-my_screen = Screen()
-timmy.shape("turtle")
-timmy.color("blue2")
-timmy.forward(100)
-print(my_screen.canvheight)
-my_screen.exitonclick()
-print("hello")
